38f8cb5f9b7877cbfe9c4009181552bacd798c30
internalFatalError42 <p.mielke87@gmx.de>
2022-06-12 01:18:20 +0200

    first commit

---
+++ fastasfuck.py
@@ -0,0 +1,36 @@
+from functools import cache, lru_cache
+import numpy as np
+import time
+
+
+#@lru_cache(maxsize=5)
+@cache
+def fib(n):
+    if n <= 1:
+        return n
+    #return fib(np.subtract(n, 1)) + fib(np.subtract(n, 2))
+    return fib(n - 1) + fib(n - 2)
+
+
+def loop():
+    for i in range(4000):
+        print(f"{i} {fib(i)}")
+    print("done")
+
+
+def main():
+    """
+    for i in range(400):
+        print(i, fib(i))
+    print("done")
+"""
+    print(time.time())
+    zeitanfang = time.time()
+    loop()
+    zeitende = time.time()
+    print("Dauer Programmausführung:", )
+    print(zeitende - zeitanfang)
+
+
+if __name__ == '__main__':
+    main()
